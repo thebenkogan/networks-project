ddf04dd710eaa1697274e479e7e60f17c95e186b
Ben Kogan <benkogan9@gmail.com>
2023-04-13 23:56:37 -0400

    use params map

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 
-RIPE_API = "https://atlas.ripe.net/api/v2/probes?status_name=Connected&tags=starlink"
+RIPE_API = "https://atlas.ripe.net/api/v2/probes"
+
+STARLINK_PARAMS = {"status_name": "Connected", "tags": "starlink"}
 
 
 def get_starlink_probe_ids():
-    response = requests.get(RIPE_API)
+    response = requests.get(RIPE_API, params=STARLINK_PARAMS)
     json = response.json()
 
     return [p["id"] for p in json["results"]]
