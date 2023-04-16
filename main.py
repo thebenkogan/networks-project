8566d201eb799baa5ce22f6254263acfdab2df30
Ben Kogan <85153105+thebenkogan@users.noreply.github.com>
2023-04-16 18:21:44 -0400

    Step 1: Get Starlink Probe IDs (#3)
    * get probe ids with example json
    
    * use list comprehension so we are more pYtHoNiC
    
    * use params map
    
    * load api key from env
    
    * filter on asn

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.environ["API_KEY"]
+RIPE_API = "https://atlas.ripe.net/api/v2/probes"
+STARLINK_PARAMS = {"status_name": "Connected", "tags": "starlink", "asn": 14593}
+
+
+def get_starlink_probe_ids():
+    response = requests.get(RIPE_API, params=STARLINK_PARAMS)
+    json = response.json()
+    return [p["id"] for p in json["results"]]
+
+
+if __name__ == "__main__":
+    print(API_KEY)
+    probe_ids = get_starlink_probe_ids()
+    print(probe_ids)
