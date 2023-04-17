49970dabae8fca3edb2a56305c71a64b6608b6a3
Ben Kogan <benkogan9@gmail.com>
2023-04-17 12:21:35 -0400

    initial attempt with ATLAS Measurement API

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 API_KEY = os.environ["API_KEY"]
 RIPE_API = "https://atlas.ripe.net/api/v2/probes"
+MEASUREMENT_API = url = 'https://atlas.ripe.net/api/v2/measurements/'
 STARLINK_PARAMS = {"status_name": "Connected", "tags": "starlink", "asn": 14593}
 
 
@@ -14,8 +15,22 @@ def get_starlink_probe_ids():
     json = response.json()
     return [p["id"] for p in json["results"]]
 
+def spawn_pings(probe_ids, target):
+    spawn_params = {
+        "definitions": [{
+            "description": "test",
+            "type": "ping",
+            "packets": 1,
+            "target": target
+        }],
+        "probes": [{"value": str(probe_id)} for probe_id in probe_ids],
+        "is_oneoff": True
+    }
+    response = requests.get(MEASUREMENT_API, params=STARLINK_PARAMS)
+    return response["measurements"]
 
 if __name__ == "__main__":
     print(API_KEY)
     probe_ids = get_starlink_probe_ids()
+    # spawn_pings(probe_ids, "")
     print(probe_ids)
