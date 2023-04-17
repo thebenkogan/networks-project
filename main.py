bb8c9364a91f6f08999d8a3e69cab9bc83dc534a
Ben Kogan <benkogan9@gmail.com>
2023-04-17 16:52:10 -0400

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 import os
-import json
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
+
 API_KEY = os.environ["API_KEY"]
 RIPE_API = "https://atlas.ripe.net/api/v2/probes"
-MEASUREMENT_API = url = 'https://atlas.ripe.net/api/v2/measurements/'
+MEASUREMENT_API = url = "https://atlas.ripe.net/api/v2/measurements/"
 STARLINK_PARAMS = {"status_name": "Connected", "tags": "starlink", "asn": 14593}
 
 
@@ -15,32 +16,35 @@ def get_starlink_probe_ids():
     json = response.json()
     return [p["id"] for p in json["results"]]
 
-def spawn_pings(probe_ids, target, key=API_KEY):
+
+def spawn_pings(probe_ids, target):
     spawn_data = {
         "definitions": [
             {
-                "target": target, 
-                "description": "fuck you gpt", 
+                "target": target,
+                "description": f"{target} pings",
                 "type": "ping",
                 "af": 4,
-                "resolve_on_probe": False,
-                "is_public": False,
+                "stop_time": str(datetime.now() + timedelta(days=1)),
+                "interval": 15 * 60,
             }
         ],
         "probes": [
             {
-                "requested": 25,
+                "requested": min(25, len(probe_ids)),
                 "type": "probes",
-                "value": ','.join([str(i) for i in probe_ids[0:25]])
+                "value": ",".join([str(i) for i in probe_ids[:25]]),
             }
-        ]
+        ],
     }
-    headers = {"Content-Type": "application/json", "Authorization": "Key {}".format(API_KEY)}
-    response = requests.post(MEASUREMENT_API, data=json.dumps(spawn_data), headers=headers)
+    headers = {
+        "Authorization": f"Key {API_KEY}",
+    }
+    response = requests.post(MEASUREMENT_API, json=spawn_data, headers=headers)
     return response.json()
 
+
 if __name__ == "__main__":
-    print(API_KEY)
     probe_ids = get_starlink_probe_ids()
-    print(spawn_pings(probe_ids, "www.reddit.com", API_KEY))
+    print(spawn_pings(probe_ids, "reddit.com"))
     print(probe_ids)
