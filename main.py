a98c87346fefe5660e94d365640d0880389e19eb
Ben Kogan <benkogan9@gmail.com>
2023-04-25 16:37:49 -0400

    4 hour ticks and formatting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import os
 import matplotlib.pyplot as plt
 import math
-from matplotlib.dates import DateFormatter
+from matplotlib.dates import DateFormatter, HourLocator
 
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
@@ -21,10 +21,19 @@ API_KEY = os.environ["API_KEY"]
 RIPE_API = "https://atlas.ripe.net/api/v2/probes"
 STARLINK_PARAMS = {"status_name": "Connected", "tags": "starlink", "asn": 14593}
 LOCATIONS = {
-    "USA1 (East Coast)" : [60510, 63017, 61780, 62417, 62553, 61081], 
-    "USA2 (Midwest and West Coast)" : [54330, 62613, 53798, 60929, 62498, 62868, 62083, 61113],
-    "EUROPE" : [61878, 62843, 60323, 1002289, 35681, 20544, 50008],
-    "OCEANIA" : [19983, 52955]
+    "USA1 (East Coast)": [60510, 63017, 61780, 62417, 62553, 61081],
+    "USA2 (Midwest and West Coast)": [
+        54330,
+        62613,
+        53798,
+        60929,
+        62498,
+        62868,
+        62083,
+        61113,
+    ],
+    "EUROPE": [61878, 62843, 60323, 1002289, 35681, 20544, 50008],
+    "OCEANIA": [19983, 52955],
 }
 
 
@@ -76,10 +85,11 @@ def fetch_ping_results(msm_id):
 
     return probe_rtts_over_time
 
+
 def create_plots(probe_data):
     # plot the graph for each key in the dictionary
     for location, probe_ids in LOCATIONS.items():
-        r, c = (math.ceil(len(probe_ids)/2), 2)
+        r, c = (math.ceil(len(probe_ids) / 2), 2)
         fig, axs = plt.subplots(r, c, constrained_layout=True)
         axs = axs.flatten()
         fig.suptitle(location)
@@ -87,19 +97,19 @@ def create_plots(probe_data):
             # get datetime objects and rtt values from the list of tuples
             x = [t[0] for t in probe_data[p]]
             y = [t[1] for t in probe_data[p]]
-            
+
             # plot the graph
-            axs[i].plot(x, y, marker='o', label=p)
+            axs[i].plot(x, y, marker="o", label=p)
 
             # set labels and legend
-            axs[i].set_xlabel('UTC Time')
-            axs[i].set_ylabel('RTT')
-            axs[i].set_title('{}'.format(p))
-            xticks = axs[i].get_xticks()
-            axs[i].xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
-            axs[i].set_xticks(xticks[::3])
+            axs[i].set_xlabel("UTC Time")
+            axs[i].set_ylabel("RTT")
+            axs[i].set_title("{}".format(p))
+            axs[i].xaxis.set_major_formatter(DateFormatter("%H:%M:%S"))
+            axs[i].xaxis.set_major_locator(HourLocator(interval=4))
     # show the plot
     plt.show()
+
 
 if __name__ == "__main__":
     # probe_ids = get_starlink_probe_ids()
